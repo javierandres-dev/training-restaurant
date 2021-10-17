2cc6fc22c7af35885ec9303946e5aa5c8e75536b
1270 <1270@holbertonschool.com>
2021-10-16 19:32:30 -0500

    Training

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
-const myModal = document.getElementById("modalNewOrder");
-const myInput = document.getElementById("inputTable");
+"use strict";
+const d = document,
+  $newOrder = d.getElementById("newOrder"),
+  $table = d.getElementById("table"),
+  $time = d.getElementById("time"),
+  $createOrder = d.getElementById("createOrder");
 
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
-});
+const customer = {
+  table: "",
+  time: "",
+  order: [],
+};
+
+$newOrder.addEventListener("shown.bs.modal", () => $table.focus());
+$createOrder.addEventListener("submit", createOrder, false);
+
+function createOrder(e) {
+  e.preventDefault();
+  if (!$createOrder.checkValidity()) e.stopPropagation();
+  else {
+    const table = $table.value,
+      time = $time.value;
+    console.log(table, time);
+  }
+  $createOrder.classList.add("was-validated");
+}
